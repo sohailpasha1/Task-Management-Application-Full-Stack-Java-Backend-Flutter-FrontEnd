d093218fbca867f45e0d099fa660c0693955a2f3
Sohail <157475077+sohailpasha1@users.noreply.github.com>
2024-04-26 00:24:23 +0530

    Add files via upload

---
+++ task_management_flutter_source/task_management_flutter_source/src/main/java/com/task/controller/TaskController.java
@@ -0,0 +1,102 @@
+package com.task.controller;
+
+import com.task.exception.DuplicateRecordException;
+import com.task.exception.InvalidRequestPayloadException;
+import com.task.exception.RecordNotFoundException;
+import com.task.service.TaskService;
+import com.task.util.ResponseBean;
+import com.task.util.TaskVO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    /**
+     * This API is use to create a new Task
+     *
+     * @param task: this parameter Task is any object which have 3 fields Title, Description and Status
+     * @return
+     * @throws DuplicateRecordException
+     */
+    @PostMapping(value = "/save-new-task")
+    public ResponseEntity<ResponseBean> saveNewTask(@RequestBody @Valid TaskVO task) throws DuplicateRecordException, InvalidRequestPayloadException {
+        return taskService.saveTask(task);
+    }
+
+    /**
+     * This API is use to get all the Task
+     *
+     * @return
+     */
+    @GetMapping(value = "/get-all")
+    public ResponseEntity<ResponseBean> getAllTask() {
+        return taskService.getAllTask();
+    }
+
+    /**
+     * This API used to Get Task By Title
+     *
+     * @param title
+     * @return
+     * @throws RecordNotFoundException
+     */
+    @GetMapping(value = "/get-by-title")
+    public ResponseEntity<ResponseBean> getTaskByTitle(@RequestParam(value = "title") String title) throws RecordNotFoundException {
+        return taskService.getTaskByTitle(title);
+    }
+
+    /**
+     * This API is used to Sort the Tasks by Status : To Do, In Progress, Done
+     *
+     * @param status
+     * @return
+     * @throws RecordNotFoundException
+     */
+    @GetMapping(value = "/sort-task-by-status")
+    public ResponseEntity<ResponseBean> sortTaskByStatus(@RequestParam(value = "status") String status) throws RecordNotFoundException, InvalidRequestPayloadException {
+        return taskService.sortTaskByStatus(status);
+    }
+
+    /**
+     * This API used to Get Task By status
+     *
+     * @param status
+     * @return
+     * @throws RecordNotFoundException
+     */
+    @GetMapping(value = "/get-by-status")
+    public ResponseEntity<ResponseBean> getTaskByStatus(@RequestParam(value = "status") String status) throws RecordNotFoundException, InvalidRequestPayloadException {
+        return taskService.getTaskByStatus(status);
+    }
+
+    /**
+     * This API used to update the Task by Title.
+     *
+     * @param task
+     * @return
+     * @throws RecordNotFoundException
+     */
+    @PutMapping(value = "/update-task")
+    public ResponseEntity<ResponseBean> updateTask(@RequestBody @Valid TaskVO task) throws RecordNotFoundException, InvalidRequestPayloadException {
+        return taskService.updateTask(task);
+    }
+
+    /**
+     * This API is used to Delete the Task by Title
+     *
+     * @param title
+     * @return
+     * @throws RecordNotFoundException
+     */
+    @GetMapping(value = "/delete-by-title")
+    public ResponseEntity<ResponseBean> deleteTaskByTitle(@RequestParam(value = "title") String title) throws RecordNotFoundException {
+        return taskService.deleteTaskByTitle(title);
+    }
+}
---
+++ task_management_flutter_source/task_management_flutter_source/src/main/java/com/task/util/TaskConstants.java
@@ -0,0 +1,19 @@
+package com.task.util;
+
+public interface TaskConstants {
+    String SUCCESS = "success";
+    Integer SUCCESS_CODE = 200;
+    String FAILED = "failed";
+    String INTERNAL_SERVER_ERROR = "Internal Server Error";
+    Integer INTERNAL_SERVER_ERROR_CODE = 500;
+
+    String RECORD_NOT_FOUND = "Record Not Found!!!";
+    String DUPLICATE_RECORD = "Duplicate Record!!";
+
+    String STATUS_SUPPORTED = "Supported status: To Do, In Progress & Done";
+
+    String TODO = "To Do";
+    String IN_PROGRESS = "In Progress";
+    String DONE = "Done";
+
+}
